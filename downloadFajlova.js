0bf76ad3278144143e973adbd41f4d55f3e554cb
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-18 10:38:22 +0200

    pobrisao

--- downloadFajlova.js
+++ downloadFajlova.js
@@ -1,28 +1,32 @@
-function DownloadFajlova(var br_ind,var loz)
+	window.appRootDirName = "prijava_ispita";
+	document.addEventListener("deviceready", onDeviceReady, false);
+	 
+	function onDeviceReady() 
 	{
-		alert("poziva se download fajlova");
-		window.appRootDirName = "prijava_ispita";
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-		
-		function fail() 
-		{
-		alert("failed to get filesystem");
-		}
-		 
-		function gotFS(fileSystem) 
-		{
-		window.fileSystem = fileSystem;
-		fileSystem.root.getDirectory(window.appRootDirName, {
-		create : true,
-		exclusive : false
-		}, dirReady, fail);
-		}
-		 
-		function dirReady(entry)
-		{
-		window.appRootDir = entry;
-		}
-		
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+	}
+	 
+	function fail() {
+	alert("failed to get filesystem");
+	}
+	 
+	function gotFS(fileSystem) {
+	alert("filesystem got");
+	window.fileSystem = fileSystem;
+	fileSystem.root.getDirectory(window.appRootDirName, {
+	create : true,
+	exclusive : false
+	}, dirReady, fail);
+	}
+	 
+	function dirReady(entry) {
+	window.appRootDir = entry;
+	alert("application dir is ready");
+	}
+	 
+	 
+	downloadFile = function(br_ind,loz)
+	{
 		var url = new Array();
 		url[0] = "http://wstest.etf.unssa.rs.ba/studenti/nepolozeni_ispiti/etf/"+br_ind+"/"+loz;
 		url[1] = "http://wstest.etf.unssa.rs.ba/studenti/polozeni_ispiti/etf/"+br_ind+"/"+loz;
@@ -30,34 +34,27 @@ function DownloadFajlova(var br_ind,var loz)
 		url[3] = "http://wstest.etf.unssa.rs.ba/studenti/status/etf/"+br_ind+"/"+loz;
 		
 		var file = new Array();
-		file[0] = "nepolozeni_ispiti.json";
-		file[1] = "polozeni_ispiti.json";
-		file[2] = "ispiti_za_polaganje.json";
-		file[3] = "status_studenta.json";
+		file[0] = "/nepolozeni_ispiti.json";
+		file[1] = "/polozeni_ispiti.json";
+		file[2] = "/ispiti_za_polaganje.json";
+		file[3] = "/status_studenta.json";
 		
 		for(var i=0;i<4;i++)
 		{
-		var adresa=url[i];
-		var ime_fajla=file[i];
+			var adresa=url[i];
+			var ime_fajla=url[i];
 			
-		 
-		downloadFile = function(adresa,ime_fajla)
-		{
-		alert("vrsi se dovnload"+ime_fajla);
-		var fileTransfer = new FileTransfer();
-		var filePath = window.appRootDir.fullPath + file;
-		fileTransfer.download(
-		url,
-		filePath,
+			var fileTransfer = new FileTransfer();
+			var filePath = window.appRootDir.fullPath + ime_fajla;
+			fileTransfer.download(
+			adresa,
+			filePath,
 			function(entry) {
 			alert("download complete: " + entry.fullPath);
 			},
-				function(error) {
-				alert("download error" + error.source);
-				}
+			function(error) {
+			alert("download error" + error.source);
+			}
 			);
 		}
-		
-		}
-   
 	}
