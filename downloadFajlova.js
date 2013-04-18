0d07ff9d906cf02b6456c3e81b99e9ebb68e5df0
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-18 12:35:53 +0300

    Update downloadFajlova.js

--- downloadFajlova.js
+++ downloadFajlova.js
@@ -1,56 +1,43 @@
-	window.appRootDirName = "prijava_ispita";
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);	 
-	function fail() {
-	alert("failed to get filesystem");
-	}
-	 
-	function gotFS(fileSystem)
-	{
-	alert("filesystem got");
-	window.fileSystem = fileSystem;
-	fileSystem.root.getDirectory(window.appRootDirName, {
-	create : true,
-	exclusive : false
-	}, dirReady, fail);
-	}
-	 
-	function dirReady(entry) {
-	window.appRootDir = entry;
-	alert("application dir is ready");
-	}
-	 
-	 
-	downloadFile = function(br_ind,loz)
-	{
-		var url = new Array();
-		url[0] = "http://wstest.etf.unssa.rs.ba/studenti/nepolozeni_ispiti/etf/"+br_ind+"/"+loz;
-		url[1] = "http://wstest.etf.unssa.rs.ba/studenti/polozeni_ispiti/etf/"+br_ind+"/"+loz;
-		url[2] = "http://wstest.etf.unssa.rs.ba/studenti/ispiti/etf/"+br_ind+"/"+loz;
-		url[3] = "http://wstest.etf.unssa.rs.ba/studenti/status/etf/"+br_ind+"/"+loz;
-		
-		var file = new Array();
-		file[0] = "/nepolozeni_ispiti.json";
-		file[1] = "/polozeni_ispiti.json";
-		file[2] = "/ispiti_za_polaganje.json";
-		file[3] = "/status_studenta.json";
-		
-		for(var i=0;i<4;i++)
-		{
-			var adresa=url[i];
-			var ime_fajla=url[i];
-			
-			var fileTransfer = new FileTransfer();
-			var filePath = window.appRootDir.fullPath + ime_fajla;
-			alert(filePath);
-			fileTransfer.download(
-			adresa,
-			filePath,
-			function(entry) {
-			alert("download complete: " + entry.fullPath);
-			},
-			function(error) {
-			alert("download error" + error.source);
-			}
-			);
-		}
-	}
+window.appRootDirName = "prijava_ispita";
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function onDeviceReady() {
+console.log("device is ready");
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
+ 
+function fail() {
+console.log("failed to get filesystem");
+}
+ 
+function gotFS(fileSystem) {
+console.log("filesystem got");
+window.fileSystem = fileSystem;
+fileSystem.root.getDirectory(window.appRootDirName, {
+create : true,
+exclusive : false
+}, dirReady, fail);
+}
+ 
+function dirReady(entry) {
+window.appRootDir = entry;
+console.log("application dir is ready");
+}
+ 
+ 
+downloadFile = function(){
+var fileTransfer = new FileTransfer();
+ 
+var url = "http://qa.etf.unssa.rs.ba/univ/statut.pdf";
+var filePath = window.appRootDir.fullPath + "/test.pdf";
+fileTransfer.download(
+url,
+filePath,
+function(entry) {
+alert("download complete: " + entry.fullPath);
+},
+function(error) {
+alert("download error" + error.source);
+}
+);
+}
