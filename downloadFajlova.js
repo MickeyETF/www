a1d55a6109fc20fe9b7c2076ff6057a841efb066
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-19 18:24:40 +0300

    Update downloadFajlova.js

--- downloadFajlova.js
+++ downloadFajlova.js
@@ -4,7 +4,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 alert("device is ready");
 window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-downloadFile(korisnicko_ime,md5_lozinka);
 }
  
 function fail() {
