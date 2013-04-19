e945f864af6bdf31b96c4e9d376287aa0a3361c7
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-19 19:08:52 +0300

    Update downloadFajlova.js

--- downloadFajlova.js
+++ downloadFajlova.js
@@ -2,7 +2,7 @@ window.appRootDirName = "prijava_ispita";
 document.addEventListener("deviceready", onDeviceReady, false);
  
 function onDeviceReady() {
-alert("device is ready");
+//alert("device is ready");
 window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 var korisnicko_ime = window.localStorage.getItem("korisnicko_ime");
 var lozinka = window.localStorage.getItem("lozinka");
@@ -19,11 +19,11 @@ if(korisnicko_ime!=null && lozinka!=null)
 }
  
 function fail() {
-alert("failed to get filesystem");
+//alert("failed to get filesystem");
 }
  
 function gotFS(fileSystem) {
-alert("filesystem got");
+//alert("filesystem got");
 window.fileSystem = fileSystem;
 fileSystem.root.getDirectory(window.appRootDirName, {
 create : true,
@@ -33,12 +33,12 @@ exclusive : false
  
 function dirReady(entry) {
 window.appRootDir = entry;
-alert("application dir is ready");
+//alert("application dir is ready");
 }
  
  
 downloadFile = function(br_ind,loz){
- alert("poziva se download"+br_ind+" "+loz)
+ //alert("poziva se download"+br_ind+" "+loz)
 
 var url = new Array();
  	url[0] = "http://wstest.etf.unssa.rs.ba/studenti/nepolozeni_ispiti/etf/"+br_ind+"/"+loz;
@@ -57,17 +57,17 @@ var url = new Array();
 			var fileTransfer = new FileTransfer();
 			var adresa=url[i].toString();
 			var ime_fajla=file[i].toString(); 
-			   alert(adresa);
-			   alert(ime_fajla);
+			   //alert(adresa);
+			   //alert(ime_fajla);
 			   var filePath = window.appRootDir.fullPath + ime_fajla;
 			   fileTransfer.download(
 			   adresa,
 			   filePath,
 			   function(entry) {
-			   alert("download complete: " + entry.fullPath);
+			   //alert("download complete: " + entry.fullPath);
 			   },
 			   function(error) {
-			   alert("download error" + error.source);
+			   //alert("download error" + error.source);
 			   }
 			   );
 			}
